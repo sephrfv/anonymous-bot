51e65e949a65639a9c1a6fac1e1232694c52c8e6
sephrfv <sepehralibeygi@gmail.com>
2026-07-04 02:18:09 +0330

    \bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+import os
 
+TOKEN = os.getenv("TOKEN")
+ADMIN_ID = 8372736522
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    await context.bot.send_message(
+        chat_id=ADMIN_ID,
+        text=f"📩 پیام جدید:\n\n{text}"
+    )
+
+    await update.message.reply_text("ارسال شد ✅")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+app.run_polling()
